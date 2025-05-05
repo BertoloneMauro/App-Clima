40de2158b3e462f80b74e10519a3ab9770a861ce
BertoloneMauro <146277778+BertoloneMauro@users.noreply.github.com>
2025-05-05 19:50:08 -0300

    Me faltó borrar una carpeta de más

--- traductor-app/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.maurobertolone.java.jdbc.traductorapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.maurobertolone.java.jdbc.traductorapp to javafx.fxml;
-    exports org.maurobertolone.java.jdbc.traductorapp;
-}
